83c5b01ece431b5e65a5dc4e8215e40ceab17c9f
ymatushevych-appwell <ymatushevych-appwell@ancestry.com>
2024-10-17 21:38:32 -0700

    docs[leetCode]: 5. Longest Palindromic Substring

--- 5.LongestPalindromicSubstring.ts
+++ 5.LongestPalindromicSubstring.ts
@@ -28,3 +28,14 @@ function longestPalindrome(s: string): string {
 
     return s.substring(start, start + maxLength)
 };
+
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+// Example 2:
+// Input: s = "cbbd"
+// Output: "bb"
